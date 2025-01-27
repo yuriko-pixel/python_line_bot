dda8e886d832f0293269e3834be959b950b5bd1f
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2025-01-27 22:36:56 +0900

    test2

--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import os
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 
 app = Flask(__name__)
 
