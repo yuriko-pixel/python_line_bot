1be762169704d163e7e13fb7a7834df681e758c4
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2025-01-27 22:35:04 +0900

    test

--- api/index.py
+++ api/index.py
@@ -1,38 +1,11 @@
-import os
-from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+from flask import Flask
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route("/callback", methods={'POST'})
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature, please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=8000)
+@app.route('/about')
+def about():
+    return 'About'
