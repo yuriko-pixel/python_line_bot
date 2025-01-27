6eede9f93c99226c761f1a60bc744900e7ff7e2f
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2025-01-27 22:59:43 +0900

    test5

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ from linebot.models import MessageEvent, TextMessage
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+line_handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
 @app.route('/')
 def home():
