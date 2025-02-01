5d654d66904316e7f593c4b63e0646a3d26d475b
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2025-02-01 17:18:30 +0900

    gemini ai

--- api/index.py
+++ api/index.py
@@ -17,11 +17,16 @@ from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
+import google.generativeai as genai
 
 app = Flask(__name__)
 
 line_handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 configuration = Configuration(access_token=os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
 
 @app.route('/')
 def home():
@@ -50,9 +55,13 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
+        user_message = event.message.text
+        ai_response = model.start_chat().send_message(user_message)
+        ai_reply_text = ai_response.text
+
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[TextMessage(text=ai_reply_text)]
             )
         )
